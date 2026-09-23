4e3d34cc9ffd35a75dacfdc8cc8d688a637af3c3
omarelghandour1993 <omar_aboelfotoh@yahoo.com>
2026-09-23 13:26:22 +0300

    Refactor server.js to use Express for routing

--- server.js
+++ server.js
@@ -1,58 +1,21 @@
-async function translateText(text, sourceLanguage, targetLanguage) {
-  const baseUrl = (
-    process.env.LIBRETRANSLATE_URL ||
-    'https://libretranslate.com'
-  ).replace(/\/+$/, '');
+'use strict';
 
-  const apiKey = process.env.LIBRETRANSLATE_API_KEY || '';
+const express = require('express');
+const path = require('path');
 
-  const requestBody = {
-    q: text,
-    source: sourceLanguage,
-    target: targetLanguage,
-    format: 'text'
-  };
+const app = express();
+const port = process.env.PORT || 3000;
 
-  if (apiKey) {
-    requestBody.api_key = apiKey;
-  }
+app.use(express.static(path.join(__dirname, 'public')));
 
-  const response = await fetch(baseUrl + '/translate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify(requestBody)
-  });
+app.get('/', function(req, res) {
+  res.sendFile(
+    path.join(__dirname, 'public', 'index.html')
+  );
+});
 
-  const responseText = await response.text();
-
-  let data;
-
-  try {
-    data = JSON.parse(responseText);
-  } catch (error) {
-    throw new Error(
-      'LibreTranslate returned an invalid response: ' +
-      responseText.substring(0, 300)
-    );
-  }
-
-  if (!response.ok) {
-    throw new Error(
-      'LibreTranslate failed with HTTP ' +
-      response.status +
-      ': ' +
-      (data.error || responseText)
-    );
-  }
-
-  if (!data.translatedText) {
-    throw new Error(
-      data.error || 'LibreTranslate returned no translated text.'
-    );
-  }
-
-  return data.translatedText;
-}
+app.listen(port, function() {
+  console.log(
+    'Axvyn is running on port ' + port
+  );
+});
